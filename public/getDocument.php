2f560680daadab5e8d8daa8f96fbbd6911da99a9
Faulo <FauloLio@gmail.com>
2017-12-28 22:10:46 +0100

    initial release

---
+++ public/getDocument.php
@@ -0,0 +1,11 @@
+<?php
+namespace Slothsoft\Farah;
+
+require_once __DIR__ . '/../farah/constants.php';
+
+$path = isset($_SERVER['PATH_INFO'])
+	? $_SERVER['PATH_INFO']
+	: '';
+$mode = HTTPDocument::LOOKUP_DOCUMENT;
+
+HTTPDocument::parseRequest($path, $mode);
---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+namespace Slothsoft\Farah;
+
+require_once __DIR__ . '/../farah/constants.php';
+
+//if (!strpos($_SERVER['REMOTE_ADDR'], ':')) die('Wartungsarbeiten... BRB /o/');
+
+$path = $_SERVER['REQUEST_URI'];
+
+$path = explode('?', $path, 2);
+$path = array_shift($path);
+
+if (substr($path, -1) !== '/' and strpos($path, '.') === false) {
+	$redirect = $path . '/';
+	header(sprintf('Location: %s', $redirect));
+	die();
+}
+
+$mode = HTTPDocument::LOOKUP_PAGE;
+
+HTTPDocument::parseRequest($path, $mode);
