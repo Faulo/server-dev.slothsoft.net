ff07b773150afef71356041b296265127f927b67
Faulo <FauloLio@gmail.com>
2018-02-21 10:35:54 +0100

    restructering

--- public/getAsset.php
+++ public/getAsset.php
@@ -4,7 +4,7 @@ namespace Slothsoft\Farah;
 require_once __DIR__ . '/../farah/constants.php';
 
 $path = isset($_SERVER['PATH_INFO'])
-	? 'farah:/' . $_SERVER['PATH_INFO']
+	? '/' . $_SERVER['PATH_INFO']
 	: '';
 $mode = Kernel::LOOKUP_ASSET;
 
